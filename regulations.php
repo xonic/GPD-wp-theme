04250268ca47b2ac18806f0b6b97d4bd27c8a278
Thomas Naegele <xonic@gmx.eu>
2013-10-26 17:53:43 +0200

    - fill content into regulations & contact page - restyle header - go flat and minimalistic on mobile - remove slider background - improve shadow on polaroids

--- regulations.php
+++ regulations.php
@@ -14,6 +14,41 @@ get_header(); ?>
 		<h1 class="section__title section__title--emphasized">
 			<? _e("pre__rules__title", "gp-dolomiti"); ?>
 		</h1>
+		<h2>
+			<? _e("pre__rules__subtitle", "gp-dolomiti"); ?>
+		</h2>
+		<dl>
+			<dt>
+				<? _e("pre__rules__a", "gp-dolomiti"); ?>
+			</dt>
+			<dd>
+				<? _e("pre__rules__a__year", "gp-dolomiti"); ?>
+			</dd>
+			<dt>
+				<? _e("pre__rules__b", "gp-dolomiti"); ?>
+			</dt>
+			<dd>
+				<? _e("pre__rules__b__year", "gp-dolomiti"); ?>
+			</dd>
+			<dt>
+				<? _e("pre__rules__c", "gp-dolomiti"); ?>
+			</dt>
+			<dd>
+				<? _e("pre__rules__c__year", "gp-dolomiti"); ?>
+			</dd>
+			<dt>
+				<? _e("pre__rules__d", "gp-dolomiti"); ?>
+			</dt>
+			<dd>
+				<? _e("pre__rules__d__year", "gp-dolomiti"); ?>
+			</dd>
+			<dt>
+				<? _e("pre__rules__e", "gp-dolomiti"); ?>
+			</dt>
+			<dd>
+				<? _e("pre__rules__e__year", "gp-dolomiti"); ?>
+			</dd>
+		</dl>
 	</div>
 </section>
 
