a61ad1bd2a6ff4d264ea92c21ad3611f135137a6
Thomas Naegele <xonic@gmx.eu>
2013-10-27 11:15:55 +0100

    - fix timetable page for ie8 - add background-size polyfill - add no-mq classes

--- header.php
+++ header.php
@@ -82,6 +82,9 @@
 			<div class="flyout">
 				<div class="grid cf">
 					<a class="flyout__toggle js-flyout__toggle" href="javascript:void(0)"><i class="icon-reorder"></i><i class="icon-remove"></i></a>
+					<!--[if lte IE 8]>
+					<img class="mountains" src="<?php echo get_template_directory_uri(); ?>/images/mountains--bw.png" alt="Gran Premio Dolomiti Logo">
+					<![endif]-->
 					<img class="mountains" src="<?php echo get_template_directory_uri(); ?>/images/mountains--bw.svg" alt="Gran Premio Dolomiti Logo" width="70" height="44">
 					<nav class="nav-main">
 						<? wp_nav_menu( array( 'theme_location' => 'nav-main' ) ); ?>
